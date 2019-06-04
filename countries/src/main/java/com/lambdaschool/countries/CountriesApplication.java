e576f21ea55b828506a55ce260c8f24bcbdc4f36
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-06-04 07:36:01 -0700

    created controller, obj types for new country lists. Having a odd bug
    Not able to start the server due to internal Kotolin or whatever its called type of error.

--- countries/src/main/java/com/lambdaschool/countries/CountriesApplication.java
+++ countries/src/main/java/com/lambdaschool/countries/CountriesApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CountriesApplication {
 
+    static CountryList ourCountryList;
+
     public static void main(String[] args) {
         SpringApplication.run(CountriesApplication.class, args);
+
+        // creates a new obj of type countryList that lets us access data we need
+        ourCountryList = new CountryList();
     }
 
 }
--- countries/src/main/java/com/lambdaschool/countries/CountryList.java
+++ countries/src/main/java/com/lambdaschool/countries/CountryList.java
@@ -3,7 +3,7 @@ package com.lambdaschool.countries;
 import java.util.ArrayList;
 
 public class CountryList {
-    
+
 
     public ArrayList<Country> countryList = new ArrayList<>();
 
